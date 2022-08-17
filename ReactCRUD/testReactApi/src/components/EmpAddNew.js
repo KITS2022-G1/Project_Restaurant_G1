a04e6b6b1391c02ba76bccfa9d41dc63efe27e1c
aston hawk <77835584+dungnguyenhd@users.noreply.github.com>
2022-08-17 11:57:46 +0700

    reup react

--- ReactCRUD/testReactApi/src/components/EmpAddNew.js
+++ ReactCRUD/testReactApi/src/components/EmpAddNew.js
@@ -9,7 +9,7 @@ import {
   MDBCheckbox
 } from 'mdb-react-ui-kit';
 
-function EmpAddNew(){
+function EmpAddNew() {
   const [branch, setBranch] = useState([]);
   // var branchNew = {};
   // branchNew.branchAddress='';
@@ -33,82 +33,101 @@ function EmpAddNew(){
   }
 
   const saveBranch = (event) => {
-    EmpServices.addNewBranch(branch).then(res =>{
+    EmpServices.addNewBranch(branch).then(res => {
       console.log('save success!');
     });
   }
 
 
-    return (
-      <div>
-        <h1> Add New Branch </h1>
-        <div className='card-body'>
-          <form>
-            <div className='form-group'>
-              <label> Branch Address: </label>
-              <input placeholder='Branch Address' name='branchAddress' className='form-control' value={branch.branchAddress} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-
-            <div className='form-group'>
-              <label> Branch Name: </label>
-              <input placeholder='Branch Name' name='branchName' className='form-control' value={branch.branchName} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-
-            <div className='form-group'>
-              <label> Branch Card Number: </label>
-              <input placeholder='Branch Card Number' name='branchCardNumber' className='form-control' value={branch.branchCardNumber} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-            <label> Branch Email: </label>
-            <input placeholder='Branch Email' name='branchEmail' className='form-control' value={branch.branchEmail} onChange={(e) => handleChange(e)}>
-            </input>
-
-            <div className='form-group'>
-              <label> Branch Image URL: </label>
-              <input placeholder='Branch Image URL' name='branchImageURL' className='form-control' value={branch.branchImageURL} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-
-            <div className='form-group'>
-              <label> Branch Manager Name: </label>
-              <input placeholder='Branch Manager Name' name='branchManagerName' className='form-control' value={branch.branchManagerName} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-
-            <div className='form-group'>
-              <label> Branch Phone: </label>
-              <input placeholder='Branch Phone' name='branchPhone' className='form-control' value={branch.branchPhone} onChange={(e) => handleChange(e)}>
-              </input>
-            </div>
-
-            <button className='btn btn-info' onClick={() => saveBranch()}> Save </button>
-            <Link to={"/"}><button className='btn btn-secondary'> Cancle </button></Link>
-
-          </form>
-        </div>
-
+  return (
+    <div className='container col-md-8'>
+      <h1> Add New Branch </h1>
       <div>
         <MDBValidation className='row g-3'>
-      <MDBValidationItem className='col-md-4'>
-      <MDBInput
-          placeholder='Branch Address'
-          name='branchAddress'
-          className='form-control'
-          value={branch.branchAddress}
-          onChange={(e) => handleChange(e)}
-        />
-        </MDBValidationItem>
-        <div className='col-12'>
-        <MDBBtn type='submit'>Submit form</MDBBtn>
-        <MDBBtn type='reset'>Reset form</MDBBtn>
-      </div>
+
+          <label> Branch Address: </label>
+          <MDBValidationItem feedback='Please provide a valid address.' invalid>
+            <MDBInput
+              placeholder='Branch Address'
+              name='branchAddress'
+              className='form-control'
+              value={branch.branchAddress}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch Name: </label>
+          <MDBValidationItem feedback='Please provide a valid name.' invalid>
+            <MDBInput
+              placeholder='Branch Name' name='branchName' className='form-control' value={branch.branchName} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch Card Number: </label>
+          <MDBValidationItem feedback='Please provide a valid card number.' invalid>
+            <MDBInput
+              placeholder='Branch Card Number' name='branchCardNumber' className='form-control' value={branch.branchCardNumber} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch Email: </label>
+          <MDBValidationItem feedback='Please provide a valid card email.' invalid>
+            <MDBInput
+              placeholder='Branch Email' name='branchEmail' className='form-control' value={branch.branchEmail} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch ImageURL: </label>
+          <MDBValidationItem feedback='Please provide a valid card email.' invalid>
+            <MDBInput
+              placeholder='Branch Image URL' name='branchImageURL' className='form-control' value={branch.branchImageURL} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch Manager Name: </label>
+          <MDBValidationItem feedback='Please provide a valid card email.' invalid>
+            <MDBInput
+              placeholder='Branch Manager Name' name='branchManagerName' className='form-control' value={branch.branchManagerName} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+
+          <label> Branch Phone: </label>
+          <MDBValidationItem feedback='Please provide a valid card email.' invalid>
+            <MDBInput
+              placeholder='Branch Phone' name='branchPhone' className='form-control' value={branch.branchPhone} onChange={(e) => handleChange(e)}
+              required
+            />
+          </MDBValidationItem>
+
+          {/* --------------------------------------------------------- */}
+          <div className='col-12'>
+            <MDBBtn type='submit' className='btn btn-info me-2' onClick={() => saveBranch()}>Save</MDBBtn>
+            <MDBBtn type='reset' className='btn btn-danger me-2'>Reset form</MDBBtn>
+            <Link to={"/"}><button className='btn btn-secondary me-2'> Cancel </button></Link>
+          </div>
         </MDBValidation>
       </div>
 
-      </div>
-    )
+    </div>
+  )
 }
 
 export default EmpAddNew;
