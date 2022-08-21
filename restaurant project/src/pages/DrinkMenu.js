920d0404a474af7eadaf36b12d1b380c0e0629e2
nguyentienlam14 <72743252+nguyentienlam14@users.noreply.github.com>
2022-08-21 21:31:27 +0700

    test
    a

--- restaurant project/src/pages/DrinkMenu.js
+++ restaurant project/src/pages/DrinkMenu.js
@@ -3,7 +3,265 @@ import React from "react";
 const DrinhMenu = () => {
   return (
     <>
-    
+    <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              RƯỢU TRẮNG
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>2019 Concha Y Toro, Sendero (Chardonnay) - Thung lũng Trung tâm, Chile</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>2019 D'arenberg, Stump Jump (Riesling) - Thung lũng Mclaren, AUS</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>De Bortoli 2018, Lựa chọn gia đình DB, (Semillon Chardonnay) - Riverina</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>2020 Sangre de Toro Verdejo</span>
+                <span class="dots"></span>
+                <span>185.000 / Ly; 920.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>17/18 Les Hauts de La Gaffeliere của Chateau la Gaffeliere, Pháp</span>
+                <span class="dots"></span>
+                <span>200.000 / Ly; 990.000 / Chai vnđ</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
+
+      <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              RƯỢU VANG ĐỎ
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>De Bortoli 2018, Lựa chọn gia đình DB, (Shiraz Cabernet) - Riverina</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Sangre de Toro (Carbernet Sauvignon)</span>
+                <span class="dots"></span>
+                <span>185.000 / Kính; 920.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>16/17 Les Hauts de La Gaffeliere của Chateau la Gaffeliere - Bordeaux, Pháp</span>
+                <span class="dots"></span>
+                <span>200.000 / Ly; 990.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Louis Jadot, Couvent de Jacobins (Pinot Noir) - Bourgogne, Pháp</span>
+                <span class="dots"></span>
+                <span>380.000 / Ly; 1.900.000 / Chai</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
+
+      <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              RƯỢU TRẮNG
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>2019 Concha Y Toro, Sendero (Chardonnay) - Thung lũng Trung tâm, Chile</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>2019 D'arenberg, Stump Jump (Riesling) - Thung lũng Mclaren, AUS</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>De Bortoli 2018, Lựa chọn gia đình DB, (Semillon Chardonnay) - Riverina</span>
+                <span class="dots"></span>
+                <span>150.000 / Ly; 750.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>2020 Sangre de Toro Verdejo</span>
+                <span class="dots"></span>
+                <span>185.000 / Ly; 920.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>17/18 Les Hauts de La Gaffeliere của Chateau la Gaffeliere, Pháp</span>
+                <span class="dots"></span>
+                <span>200.000 / Ly; 990.000 / Chai vnđ</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
+
+      <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              CHAMPAGNE BY THE GLASS
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>Delamotte Brut</span>
+                <span class="dots"></span>
+                <span>590.000 / Kính; 2.490.000 / Chai</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Prosecco</span>
+                <span class="dots"></span>
+                <span>900.000 / Chai</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
+
+      <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              KOMBUCHA
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>Táo xanh Kombucha</span>
+                <span class="dots"></span>
+                <span>75.000 vnđ</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Đào Kombucha</span>
+                <span class="dots"></span>
+                <span>75.000 vnđ</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Vải thiều Kombucha</span>
+                <span class="dots"></span>
+                <span>75.000 vnđ</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Vải thiều Kombucha</span>
+                <span class="dots"></span>
+                <span>90,000 vnđ</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Xoài hồng</span>
+                <span class="dots"></span>
+                <span>90,000 vnđ</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
+      
+      <section style={{ padding: "3rem" }}>
+        <div className="row text-menu">
+          <div className="col-2"></div>
+          <div className="text-center col-8">
+            <p
+              class="fs-2"
+              style={{
+                fontWeight: "bold",
+                textDecoration: "underline #2A606D wavy",
+              }}
+            >
+              COCKTAIL CỔ ĐIỂN
+            </p>
+            <ul class="leaders">
+              <li className="fs-6 pt-2 mt-3">
+                <span>Negroni</span>
+                <span class="dots"></span>
+                <span>180,000 VND</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Margarita</span>
+                <span class="dots"></span>
+                <span>180,000 VND </span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Martini</span>
+                <span class="dots"></span>
+                <span>180,000 VND</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Mojito</span>
+                <span class="dots"></span>
+                <span>180,000 VND</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>French Bellini</span>
+                <span class="dots"></span>
+                <span>250,000 VND</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Kir Royal</span>
+                <span class="dots"></span>
+                <span>250,000 VND</span>
+              </li>
+              <li className="fs-6 pt-2 mt-3">
+                <span>Mimosa</span>
+                <span class="dots"></span>
+                <span>250,000 VND</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </section>
     </>
   );
 };
