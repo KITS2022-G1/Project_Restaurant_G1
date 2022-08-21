43152a3d3c2a5ae07797a2c18a91a311fb259c23
nguyentienlam14 <72743252+nguyentienlam14@users.noreply.github.com>
2022-08-21 19:03:25 +0700

    a
    a

--- restaurant project/src/App.js
+++ restaurant project/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import Layout from './pages/Layout';
-import Home from './pages/Home';
-import About from './pages/About'
-import Menu from './pages/Menu'
-import MainCourseMenu from './pages/MainCourseMenu'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import MainCourseMenu from "./pages/MainCourseMenu";
+import SideDishMenu from "./pages/SideDishMenu";
+import DessertMenu from "./pages/DessertMenu";
+import DrinkMenu from "./pages/DrinkMenu";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
@@ -13,7 +16,10 @@ function App() {
           <Route index element={<Home />} />
           <Route path="About" element={<About />} />
           <Route path="/Menu" element={<Menu />} />
-          <Route path="/MainCourseMenu" element={<MainCourseMenu/>} />
+          <Route path="/MainCourseMenu" element={<MainCourseMenu />} />
+          <Route path="/SideDishMenu" element={<SideDishMenu />} />
+          <Route path="/DessertMenu" element={<DessertMenu />} />
+          <Route path="/DrinkMenu" element={<DrinkMenu />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- restaurant project/src/pages/DessertMenu.js
+++ restaurant project/src/pages/DessertMenu.js
@@ -1,7 +1,6 @@
 import React from "react";
-import "../css/MainCourseMenu.css";
 
-const MainCourseMenu = () => {
+const DessertMenu = () => {
   return (
     <>
       <section style={{ padding: "3rem" }}>
@@ -70,71 +69,6 @@ const MainCourseMenu = () => {
       </section>
 
       <section style={{ padding: "3rem" }}>
-        <div className="row text-menu">
-          <div className="col-2"></div>
-          <div className="text-center col-8">
-            <p
-              class="fs-1"
-              style={{
-                fontWeight: "bold",
-                textDecoration: "underline #2A606D wavy",
-              }}
-            >
-              Các món về hải sản
-            </p>
-            <ul class="leaders">
-              <li className="fs-6 pt-2">
-                <span>Cá Hồi Nướng Soup Bí Đỏ Phủ Phô Mai</span>
-                <span class="dots"></span>
-                <span>420,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Súp nghêu</span>
-                <span class="dots"></span>
-                <span>300,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Tôm Áp Chảo Với Khoai Tím</span>
-                <span class="dots"></span>
-                <span>120,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Bò hầm rau củ</span>
-                <span class="dots"></span>
-                <span>180,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Bò carpaccio</span>
-                <span class="dots"></span>
-                <span>215,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Hamburger bò phô mai</span>
-                <span class="dots"></span>
-                <span>65,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Bò cuộn thập cẩm nướng</span>
-                <span class="dots"></span>
-                <span>80,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Bò lúc lắc</span>
-                <span class="dots"></span>
-                <span>100,000 vnđ</span>
-              </li>
-              <li className="fs-6 pt-2">
-                <span>Bò hầm rượu vang</span>
-                <span class="dots"></span>
-                <span>210,000 vnđ</span>
-              </li>
-            </ul>
-          </div>
-          <div className="col-2"></div>
-        </div>
-      </section>
-
-      <section style={{ padding: "3rem" }}>
         <p
           className="fs-2 text-center"
           style={{ fontFamily: "Dancing Script, cursive", fontWeight: "bold" }}
@@ -145,4 +79,5 @@ const MainCourseMenu = () => {
     </>
   );
 };
-export default MainCourseMenu;
+
+export default DessertMenu;
